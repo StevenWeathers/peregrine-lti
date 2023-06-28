676c16e8ddb93ba1ec2b595cc5e22f069b95ccab
Steven Weathers <steven@weathers.me>
2023-06-28 09:54:08 -0400

    Move ToolDataRepo interface below structs

--- peregrine/lti.go
+++ peregrine/lti.go
@@ -5,56 +5,6 @@ import (
 	"time"
 )
 
-// ToolDataRepo is intended to be a storage (e.g. DB) service for an LTI Tools registration and launch
-type ToolDataRepo interface {
-	// GetPlatform should return a Platform by ID
-	GetPlatform(id uuid.UUID) (Platform, error)
-	// CreatePlatform should create a Platform returning Platform with ID
-	CreatePlatform(platform Platform) (Platform, error)
-	// UpdatePlatform should update a Platform by ID
-	UpdatePlatform(platform Platform) (Platform, error)
-	// DeletePlatform should delete a Platform by ID
-	DeletePlatform(id uuid.UUID) (Platform, error)
-	// GetPlatformInstance should return a PlatformInstance by ID
-	GetPlatformInstance(id uuid.UUID) (PlatformInstance, error)
-	// GetPlatformInstanceByGUID should return a PlatformInstance by GUID
-	GetPlatformInstanceByGUID(guid string) (PlatformInstance, error)
-	// CreatePlatformInstance should create a PlatformInstance returning PlatformInstance with ID
-	CreatePlatformInstance(platIns PlatformInstance) (PlatformInstance, error)
-	// UpdatePlatformInstance should update a PlatformInstance by ID
-	UpdatePlatformInstance(platIns PlatformInstance) (PlatformInstance, error)
-	// DeletePlatformInstance should delete a PlatformInstance by ID
-	DeletePlatformInstance(id uuid.UUID) error
-	// GetRegistration should return a Registration by ID
-	GetRegistration(id uuid.UUID) (Registration, error)
-	// GetRegistrationByClientID should return a Registration by ClientID
-	GetRegistrationByClientID(clientId string) (Registration, error)
-	// CreateRegistration should create a Registration returning Registration with ID
-	CreateRegistration(reg Registration) (Registration, error)
-	// UpdateRegistration should update a Registration by ID
-	UpdateRegistration(reg Registration) (Registration, error)
-	// DeleteRegistration should delete a Registration by ID
-	DeleteRegistration(id uuid.UUID) (Registration, error)
-	// GetDeployment should return a Deployment by ID
-	GetDeployment(id uuid.UUID) (Deployment, error)
-	// GetDeploymentByPlatformDeploymentID should return a Deployment by PlatformDeploymentID
-	GetDeploymentByPlatformDeploymentID(deploymentId string) (Deployment, error)
-	// CreateDeployment should create a Deployment returning Deployment with ID
-	CreateDeployment(dep Deployment) (Deployment, error)
-	// UpdateDeployment should update a Deployment by ID
-	UpdateDeployment(dep Deployment) (Deployment, error)
-	// DeleteDeployment should delete a Deployment by ID
-	DeleteDeployment(id uuid.UUID) (Deployment, error)
-	// GetLaunch should return a Launch by ID
-	GetLaunch(id uuid.UUID) (Launch, error)
-	// CreateLaunch should create a Launch returning Launch with ID
-	CreateLaunch(launch Launch) (Launch, error)
-	// UpdateLaunch should update a Launch by ID
-	UpdateLaunch(launch Launch) (Launch, error)
-	// DeleteLaunch should delete a Launch by ID
-	DeleteLaunch(id uuid.UUID) error
-}
-
 // Platform represents the LMS Platform unique by Issuer
 type Platform struct {
 	// ID (REQUIRED) is the tools UUID for the Platform
@@ -68,7 +18,8 @@ type Platform struct {
 }
 
 // PlatformInstance composes properties associated with the platform instance initiating the launch
-// optional https://purl.imsglobal.org/spec/lti/claim/tool_platform claim
+// optional https://purl.imsglobal.org/spec/lti/claim/tool_platform claim.
+//
 // In a multi-tenancy case, a single platform (iss) will host multiple instances,
 // but each LTI message is originating from a single instance identified by its guid.
 type PlatformInstance struct {
@@ -139,5 +90,55 @@ type Launch struct {
 	PlatformInstance *PlatformInstance
 	// Used (OPTIONAL) is the timestamp of when the Launch was completed, upon completion the
 	// Launch should not be reusable (whether querying by Nonce or ID)
-	Used time.Time
+	Used *time.Time
+}
+
+// ToolDataRepo is intended to be a storage (e.g. DB) service for an LTI Tools registration and launch
+type ToolDataRepo interface {
+	// GetPlatform should return a Platform by ID
+	GetPlatform(id uuid.UUID) (Platform, error)
+	// CreatePlatform should create a Platform returning Platform with ID
+	CreatePlatform(platform Platform) (Platform, error)
+	// UpdatePlatform should update a Platform by ID
+	UpdatePlatform(platform Platform) (Platform, error)
+	// DeletePlatform should delete a Platform by ID
+	DeletePlatform(id uuid.UUID) (Platform, error)
+	// GetPlatformInstance should return a PlatformInstance by ID
+	GetPlatformInstance(id uuid.UUID) (PlatformInstance, error)
+	// GetPlatformInstanceByGUID should return a PlatformInstance by GUID
+	GetPlatformInstanceByGUID(guid string) (PlatformInstance, error)
+	// CreatePlatformInstance should create a PlatformInstance returning PlatformInstance with ID
+	CreatePlatformInstance(platIns PlatformInstance) (PlatformInstance, error)
+	// UpdatePlatformInstance should update a PlatformInstance by ID
+	UpdatePlatformInstance(platIns PlatformInstance) (PlatformInstance, error)
+	// DeletePlatformInstance should delete a PlatformInstance by ID
+	DeletePlatformInstance(id uuid.UUID) error
+	// GetRegistration should return a Registration by ID
+	GetRegistration(id uuid.UUID) (Registration, error)
+	// GetRegistrationByClientID should return a Registration by ClientID
+	GetRegistrationByClientID(clientId string) (Registration, error)
+	// CreateRegistration should create a Registration returning Registration with ID
+	CreateRegistration(reg Registration) (Registration, error)
+	// UpdateRegistration should update a Registration by ID
+	UpdateRegistration(reg Registration) (Registration, error)
+	// DeleteRegistration should delete a Registration by ID
+	DeleteRegistration(id uuid.UUID) (Registration, error)
+	// GetDeployment should return a Deployment by ID
+	GetDeployment(id uuid.UUID) (Deployment, error)
+	// GetDeploymentByPlatformDeploymentID should return a Deployment by PlatformDeploymentID
+	GetDeploymentByPlatformDeploymentID(deploymentId string) (Deployment, error)
+	// CreateDeployment should create a Deployment returning Deployment with ID
+	CreateDeployment(dep Deployment) (Deployment, error)
+	// UpdateDeployment should update a Deployment by ID
+	UpdateDeployment(dep Deployment) (Deployment, error)
+	// DeleteDeployment should delete a Deployment by ID
+	DeleteDeployment(id uuid.UUID) (Deployment, error)
+	// GetLaunch should return a Launch by ID
+	GetLaunch(id uuid.UUID) (Launch, error)
+	// CreateLaunch should create a Launch returning Launch with ID
+	CreateLaunch(launch Launch) (Launch, error)
+	// UpdateLaunch should update a Launch by ID
+	UpdateLaunch(launch Launch) (Launch, error)
+	// DeleteLaunch should delete a Launch by ID
+	DeleteLaunch(id uuid.UUID) error
 }
