56b5c4c974f6cdd9eb652cd51b118a03db8dee19
Steven Weathers <steven@weathers.me>
2023-06-29 08:36:39 -0400

    Add tool platform instance check and update launch in callback

--- launch/validations.go
+++ launch/validations.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/mitchellh/mapstructure"
+
 	"github.com/stevenweathers/peregrine-lti/peregrine"
 
 	"github.com/google/uuid"
@@ -96,66 +98,78 @@ func (s *Service) validateState(state string) (uuid.UUID, error) {
 
 // parseIDToken validates the id_token jwt with the peregrine.Platform keyset returning peregrine.LTI1p3Claims
 func (s *Service) parseIDToken(ctx context.Context, launch peregrine.Launch, idToken string) (peregrine.LTI1p3Claims, error) {
-	idt := peregrine.LTI1p3Claims{}
+	var lti1p3Claims peregrine.LTI1p3Claims
 	keysetUrl := launch.Registration.Platform.KeySetURL
 
 	keySet, err := s.getPlatformJWKs(ctx, keysetUrl)
 	if err != nil {
-		return idt, fmt.Errorf("unable to retrieve %s keyset: %v", keysetUrl, err)
+		return lti1p3Claims, fmt.Errorf("unable to retrieve %s keyset: %v", keysetUrl, err)
 	}
 
 	// validate that the id_token jwt is can be parsed and return a verified token
 	// including verifying the issuer and client_id from peregrine.Launch.Platform
-	verifiedToken, err := jwt.Parse(
-		[]byte(idToken), jwt.WithKeySet(keySet), jwt.WithIssuer(launch.Registration.Platform.Issuer),
+	// as well as checking for LTI 1.3 required claims
+	verifiedToken, err := jwt.Parse([]byte(idToken), jwt.WithKeySet(keySet),
+		jwt.WithIssuer(launch.Registration.Platform.Issuer),
 		jwt.WithAudience(launch.Registration.ClientID),
+		jwt.WithClaimValue("nonce", launch.Nonce.String()),
 		jwt.WithRequiredClaim("https://purl.imsglobal.org/spec/lti/claim/deployment_id"),
+		jwt.WithClaimValue("https://purl.imsglobal.org/spec/lti/claim/message_type", "LtiResourceLinkRequest"),
+		jwt.WithClaimValue("https://purl.imsglobal.org/spec/lti/claim/version", "1.3.0"),
+		jwt.WithRequiredClaim("https://purl.imsglobal.org/spec/lti/claim/target_link_uri"),
 	)
 	if err != nil {
-		fmt.Printf("failed to verify JWS: %s\n", err)
-		return idt, err
+		return lti1p3Claims, fmt.Errorf("invalid id_token")
 	}
-	claims := verifiedToken.PrivateClaims()
 
-	// validate that nonce is in the id_token and matches peregrine.Launch nonce
-	nonce, ok := claims["nonce"]
-	if !ok {
-		return idt, fmt.Errorf("nonce missing from id_token")
+	cfg := &mapstructure.DecoderConfig{
+		Metadata: nil,
+		Result:   &lti1p3Claims,
+		TagName:  "json",
 	}
-	if nonce.(string) != launch.Nonce.String() {
-		return idt, fmt.Errorf(
-			"id_token nonce %s does not match launch nonce %s",
-			nonce.(string), launch.Nonce.String(),
-		)
+	decoder, _ := mapstructure.NewDecoder(cfg)
+	err = decoder.Decode(verifiedToken.PrivateClaims())
+	if err != nil {
+		return lti1p3Claims, fmt.Errorf("failed to decode LTI claims %v", err)
 	}
 
 	// @TODO - how to handle azp?
 
-	sub, subExist := claims["sub"]
-	if subExist && (len(sub.(string)) > 255) {
-		return idt, fmt.Errorf("sub %s in id_token exceeds 255 characters", sub.(string))
+	if lti1p3Claims.SUB != "" && (len(lti1p3Claims.SUB) > 255) {
+		return lti1p3Claims, fmt.Errorf("sub %s in id_token exceeds 255 characters", lti1p3Claims.SUB)
 	}
 
 	// validate deployment_id exists and if launch had deployment_id that it matches
-	deploymentId := claims["https://purl.imsglobal.org/spec/lti/claim/deployment_id"].(string)
-	if launch.Deployment != nil && deploymentId != launch.Deployment.ID.String() {
-		return idt, fmt.Errorf(
+	if launch.Deployment != nil && lti1p3Claims.DeploymentID != launch.Deployment.ID.String() {
+		return lti1p3Claims, fmt.Errorf(
 			"launch deployment_id %s does not match id_token deployment_id %s",
-			launch.Deployment.ID.String(), deploymentId,
+			launch.Deployment.ID.String(), lti1p3Claims.DeploymentID,
 		)
 	} else {
-		_, err := s.dataSvc.GetDeploymentByPlatformDeploymentID(ctx, deploymentId)
+		deployment, err := s.dataSvc.GetDeploymentByPlatformDeploymentID(ctx, lti1p3Claims.DeploymentID)
 		if err != nil {
-			return idt, fmt.Errorf(
+			return lti1p3Claims, fmt.Errorf(
 				"lms deployment_id %s not found in tool lti data source",
-				deploymentId,
+				lti1p3Claims.DeploymentID,
 			)
 		}
+		launch.Deployment = &deployment
 	}
 
-	// @TODO - get peregrine.PlatformInstance and validate guid against id_token claim
-	// @TODO - update launch with peregrine.Deployment ID and peregrine.PlatformInstance ID
-	// @TODO - parse LTI claims into response struct
+	if lti1p3Claims.ToolPlatform.GUID != "" {
+		platformInstance, err := s.dataSvc.GetPlatformInstanceByGUID(ctx, lti1p3Claims.ToolPlatform.GUID)
+		if err != nil {
+			return lti1p3Claims, err
+		}
+		launch.PlatformInstance = &platformInstance
+	}
 
-	return idt, nil
+	used := time.Now()
+	launch.Used = &used
+	_, err = s.dataSvc.UpdateLaunch(ctx, launch)
+	if err != nil {
+		return lti1p3Claims, err
+	}
+
+	return lti1p3Claims, nil
 }
