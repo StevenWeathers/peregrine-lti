3f418c65a58bb971b6bb4ec751656e7bb0865db0
Steven Weathers <steven@weathers.me>
2023-06-28 22:52:41 -0400

    Update HandleOidcLogin with validations and response building

--- launch/launch.go
+++ launch/launch.go
@@ -2,6 +2,7 @@ package launch
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/lestrrat-go/jwx/v2/jwk"
 	"github.com/stevenweathers/peregrine-lti/peregrine"
@@ -21,8 +22,60 @@ func New(config Config, dataSvc peregrine.ToolDataRepo) *Service {
 // HandleOidcLogin receives the peregrine.OIDCLoginRequestParams
 // then validates the request and builds the peregrine.OIDCLoginResponseParams
 // to send the Platform in the redirect to peregrine.Platform AuthLoginURL
-func (s *Service) HandleOidcLogin(params peregrine.OIDCLoginRequestParams) (peregrine.OIDCLoginResponseParams, error) {
-	resp := peregrine.OIDCLoginResponseParams{}
+func (s *Service) HandleOidcLogin(ctx context.Context, params peregrine.OIDCLoginRequestParams) (peregrine.OIDCLoginResponseParams, error) {
+	var deployment *peregrine.Deployment
+	resp := peregrine.OIDCLoginResponseParams{
+		Scope:          "openid",
+		ResponseType:   "id_token",
+		ResponseMode:   "form_post",
+		Prompt:         "none",
+		ClientID:       params.ClientID,
+		LoginHint:      params.LoginHint,
+		LTIMessageHint: params.LTIMessageHint,
+	}
+
+	err := validateLoginRequestParams(params)
+	if err != nil {
+		return resp, err
+	}
+
+	registration, err := s.dataSvc.GetRegistrationByClientID(ctx, params.ClientID)
+	if err != nil {
+		return resp, err
+	}
+
+	if params.Issuer != registration.Platform.Issuer {
+		return resp, fmt.Errorf(
+			"request issuer %s does not match registration issuer %s",
+			params.Issuer, registration.Platform.Issuer,
+		)
+	}
+
+	if params.LTIDeploymentID != "" {
+		dep, err := s.dataSvc.GetDeploymentByPlatformDeploymentID(ctx, params.LTIDeploymentID)
+		if err != nil {
+			return resp, fmt.Errorf(
+				"lms deployment_id %s not found in tool lti data source",
+				params.LTIDeploymentID,
+			)
+		}
+		deployment = &dep
+	}
+
+	launch, err := s.dataSvc.CreateLaunch(ctx, peregrine.Launch{
+		Registration: &registration,
+		Deployment:   deployment,
+	})
+	if err != nil {
+		return resp, err
+	}
+	resp.Nonce = launch.Nonce.String()
+
+	state, err := s.createLaunchState(launch.ID)
+	if err != nil {
+		return resp, err
+	}
+	resp.State = state
 
 	return resp, nil
 }
